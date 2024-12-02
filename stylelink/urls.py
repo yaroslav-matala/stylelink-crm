1e376d3c266b88d15bce14e4da0d5fd277eab714
Yaroslav Matala <s9682642@gmail.com>
2024-12-02 21:50:56 +0000

    url bugfix

--- stylelink/urls.py
+++ stylelink/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_app.urls'), name='home')
+    path('', include('client_manager.urls'), name='home')
 ]
