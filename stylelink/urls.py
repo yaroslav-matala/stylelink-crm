c0a8f55bd998a50766b2fb2e303687a1ed8ac323
Yaroslav Matala <s9682642@gmail.com>
2024-12-03 21:45:20 +0000

    install allauth package

--- stylelink/urls.py
+++ stylelink/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('client_manager.urls'), name='home'),
+    path('accounts/', include('allauth.urls')),
 ]
