1213887c0e6b9f29322330d5f379f21d38987ff4
Yaroslav Matala <s9682642@gmail.com>
2024-12-04 04:50:58 +0000

    make some fields in client form not required to fill

--- client_manager/models.py
+++ client_manager/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class Client(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
-    location = models.CharField(max_length=300)
-    formula = models.TextField()
-    notes = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=300, blank=True)
+    formula = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clients')
 
     def __str__(self):
