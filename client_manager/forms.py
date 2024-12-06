5b4cdc3bd2248068f8408d0ef3378de0ca2d764b
Yaroslav Matala <s9682642@gmail.com>
2024-12-06 04:23:29 +0000

    add placeholders to forms

--- client_manager/forms.py
+++ client_manager/forms.py
@@ -9,9 +9,27 @@ class AddClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'email', 'phone', 'location', 'formula', 'notes', 'price']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Client Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'example@example.com'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Enter Client Phone Number'}),
+            'location': forms.TextInput(attrs={'placeholder': 'Enter Client Location'}),
+            'formula': forms.Textarea(attrs={'placeholder': 'Enter Formula (optional)'}),
+            'notes': forms.Textarea(attrs={'placeholder': 'Enter Notes (optional)'}),
+            'price': forms.NumberInput(attrs={'placeholder': 'Enter Price'}),
+        }
 
 # - Update a client
 class UpdateClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'email', 'phone', 'location', 'formula', 'notes', 'price']
+        idgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Client Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'example@example.com'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Enter Client Phone Number'}),
+            'location': forms.TextInput(attrs={'placeholder': 'Enter Client Location'}),
+            'formula': forms.Textarea(attrs={'placeholder': 'Enter Formula'}),
+            'notes': forms.Textarea(attrs={'placeholder': 'Enter Notes'}),
+            'price': forms.NumberInput(attrs={'placeholder': 'Enter Price'}),
+        }
