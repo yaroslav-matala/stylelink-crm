b9769fac627a1cf2ff4deb5384a851c9246d721a
Yaroslav Matala <s9682642@gmail.com>
2024-12-06 20:08:57 +0000

    make table rows clickable

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,16 @@
 let message_timeout = document.getElementById("message-timer");
 
 setTimeout(function() {
-    message_timeout.style.display = "none";
+    if (message_timeout) {
+        message_timeout.style.display = "none";
+    }
 }, 5000);
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listener to make rows clickable
+    document.querySelectorAll('.clickable-row').forEach(row => {
+        row.addEventListener('click', function() {
+            window.location = this.getAttribute('data-href');
+        });
+    });
+});
